0e2aca7ddcf7b605b047abde3fc481273215635a
Daniel Perez <hellodan-256@outlook.com>
2026-05-18 17:16:37 -0400

    Successfully Implemented "NLL" and "TRM" Instructions

--- src/main.c
+++ src/main.c
@@ -3,10 +3,11 @@
 //  Written by Daniel Perez.
 */
 
+
 #include <stdio.h>
 #include <stdint.h>
 #define DIM_MEMORY_MAXIMUM (1 << 24)
-#define DIM_MEMORY_INSTALL (1 << 8)
+#define DIM_MEMORY_INSTALL (1 << 2)
 #define DIM_MEMORY_INBOUND(increment) (increment < DIM_MEMORY_INSTALL && increment < DIM_MEMORY_MAXIMUM)
 
 uint16_t register_a;
@@ -18,7 +19,8 @@ uint8_t  register_f;
 
 uint8_t  memory[DIM_MEMORY_INSTALL];
 
-void DIM_reset (void) {
+
+void DIM_fullReset (void) {
   
   uint16_t register_a = 0x0000;
   uint16_t register_b = 0x0000;
@@ -34,22 +36,69 @@ void DIM_reset (void) {
   }
 }
 
-void DIM_memory_dump (void) {
+void DIM_memoryDump (void) {
 
   uint32_t increment = 0x00000000;
   while (DIM_MEMORY_INBOUND(increment)) {
-      printf("0x%06X  >>  0x%02X \n", increment, memory[increment]);
-      increment += 0x01;
+
+    if ((increment % 16) == 0x00 && increment > 0x00) {
+      printf("\n0x%06X  >>  ", increment);
+    } else if ((increment % 16) == 0x00) {
+      printf("0x%06X  >>  ", increment);      
+    }
+
+    printf("%02X ", increment[memory]);
+    increment += 0x01;
+      
   }
+
+  printf("\n");
+  
+}
+
+void DIM_tick (void) {
+
+    uint8_t extracted_opcode = (memory[register_e] & 0xFC) >> 0x02;
+    uint8_t extracted_select = (memory[register_e] & 0x03);
+
+    printf("OP: 0x%02X  II: 0x%02X  FR: 0b%08B  EO: 0x%08X  ES: 0x%08X\n", memory[register_e], register_e, register_f, extracted_opcode, extracted_select);
+
+    switch (extracted_opcode) {
+
+      case (0x00):
+        //  "NLL":
+        register_e += 0x01;
+        break;
+
+      case (0x01):
+        //  "TRM":
+        register_f |= 0x01;
+        break;
+
+      default:
+      //  "NLL":
+        register_e += 0x01;
+        break;
+      
+    }
   
 }
 
 
 int main (void) {
 
-  DIM_reset();
-  DIM_memory_dump();
+  DIM_fullReset();
 
+  //  Higher six bits are the instruction.
+  //  Lower two bits is the register selection.
+  memory[0x00] = 0b00000100;
+
+  DIM_memoryDump();
+
+  while (DIM_MEMORY_INBOUND(register_e) && (!(register_f & 0x01))) {
+    DIM_tick();
+  }
+  
   return 0x00;
   
 }
