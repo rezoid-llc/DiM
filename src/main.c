13504bc9dd58ff6025c96734df41def74bef451b
Daniel Perez <hellodan-256@outlook.com>
2026-05-18 20:48:25 -0400

    Implemented "LWI" Instruction & General Fixes

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #define DIM_MEMORY_MAXIMUM (1 << 24)
 #define DIM_MEMORY_INSTALL (1 << 2)
 #define DIM_MEMORY_INBOUND(increment) (increment < DIM_MEMORY_INSTALL && increment < DIM_MEMORY_MAXIMUM)
+#define DIM_MEMORY_WORD(byte_01, byte_02) ((uint16_t)byte_01 << 8) | byte_02
 
 uint16_t register_a;
 uint16_t register_b;
@@ -22,12 +23,12 @@ uint8_t  memory[DIM_MEMORY_INSTALL];
 
 void DIM_fullReset (void) {
   
-  uint16_t register_a = 0x0000;
-  uint16_t register_b = 0x0000;
-  uint16_t register_c = 0x0000;
-  uint16_t register_d = 0x0000;
-  uint32_t register_e = 0x00000000;
-  uint8_t  register_f = 0x00;
+  register_a = 0x0000;
+  register_b = 0x0000;
+  register_c = 0x0000;
+  register_d = 0x0000;
+  register_e = 0x00000000;
+  register_f = 0x00;
 
   uint32_t increment  = 0x00000000;
   while (DIM_MEMORY_INBOUND(increment)) {
@@ -39,6 +40,7 @@ void DIM_fullReset (void) {
 void DIM_memoryDump (void) {
 
   uint32_t increment = 0x00000000;
+  printf("MEMORY DUMP\n");
   while (DIM_MEMORY_INBOUND(increment)) {
 
     if ((increment % 16) == 0x00 && increment > 0x00) {
@@ -47,7 +49,7 @@ void DIM_memoryDump (void) {
       printf("0x%06X  >>  ", increment);      
     }
 
-    printf("%02X ", increment[memory]);
+    printf("%02X ", memory[increment]);
     increment += 0x01;
       
   }
@@ -56,26 +58,46 @@ void DIM_memoryDump (void) {
   
 }
 
+void DIM_registerDump (void) {
+  printf("REGISTER DUMP\n");
+  printf("RA: 0x%04X\n", register_a);
+  printf("RB: 0x%04X\n", register_b);
+  printf("RC: 0x%04X\n", register_c);
+  printf("RD: 0x%04X\n", register_d);
+  printf("RE: 0x%06X\n", register_e);
+  printf("RF: 0b%08B\n", register_f);
+}
+
 void DIM_tick (void) {
 
-    uint8_t extracted_opcode = (memory[register_e] & 0xFC) >> 0x02;
-    uint8_t extracted_select = (memory[register_e] & 0x03);
+    uint8_t opcode = (memory[register_e] & 0xFC) >> 0x02;
+    uint8_t select = (memory[register_e] & 0x03);
 
-    printf("OP: 0x%02X  II: 0x%02X  FR: 0b%08B  EO: 0x%08X  ES: 0x%08X\n", memory[register_e], register_e, register_f, extracted_opcode, extracted_select);
+    printf("OP: 0x%02X  RE: 0x%02X  SB: 0b%08B  IO: 0x%08X  IS: 0x%08X\n", memory[register_e], register_e, register_f, opcode, select);
 
-    switch (extracted_opcode) {
+    switch (opcode) {
 
-      case (0x00):
+      case (0x00):;
         //  "NLL":
         register_e += 0x01;
         break;
 
-      case (0x01):
+      case (0x01):;
         //  "TRM":
         register_f |= 0x01;
         break;
 
-      default:
+      case (0x08):;
+        //  "LWI"
+        uint16_t immediate = DIM_MEMORY_WORD(memory[register_e + 0x01], memory[register_e + 0x02]);
+        if (select == 0x00) { register_a = immediate;
+        } else if (select == 0x01) { register_b = immediate;
+        } else if (select == 0x02) { register_c = immediate;
+        } else if (select == 0x03) { register_d = immediate;
+        } register_e += 0x03;
+        break;
+
+      default:;
       //  "NLL":
         register_e += 0x01;
         break;
@@ -91,13 +113,19 @@ int main (void) {
 
   //  Higher six bits are the instruction.
   //  Lower two bits is the register selection.
-  memory[0x00] = 0b00000100;
+  memory[0x00] = 0b00100000;
+  memory[0x01] = 0xFF;
+  memory[0x02] = 0xFF;
+  memory[0x03] = 0b00000100;
 
   DIM_memoryDump();
 
+  printf("EXECUTION STARTED:\n");
   while (DIM_MEMORY_INBOUND(register_e) && (!(register_f & 0x01))) {
     DIM_tick();
   }
+
+  DIM_registerDump();
   
   return 0x00;
   
